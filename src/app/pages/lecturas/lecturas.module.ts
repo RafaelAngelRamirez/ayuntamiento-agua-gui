c7f3048c38042228c0e5c24e8d023e5320b876d6
Rafael Ramirez <rafa.yael@gmail.com>
2020-08-24 13:04:41 -0500

    Componente funcionando de nuevo

--- src/app/pages/lecturas/lecturas.module.ts
+++ src/app/pages/lecturas/lecturas.module.ts
@@ -1,21 +1,50 @@
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LecturasComponent } from "./lecturas.component";
 import { RouterModule, Routes } from "@angular/router";
+import { LecturaCrearComponent } from "./lectura-crear/lectura-crear.component";
+import { ReactiveErrorHandlerModule } from "@codice-progressio/reactive-error-handler";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser'
 
 const routes: Routes = [
   {
     path: "",
     data: {
-      title: "Lectura",
+      title: "",
       urls: [{ title: "Lectura", url: "/lectura" }, { title: "Lectura" }],
     },
     component: LecturasComponent,
   },
+  {
+    path: "captura/:contrato",
+    // data: {
+    //   title: "",
+    //   urls: [
+    //     { title: "Lectura", url: "/lectura/captura/:id" },
+    //     { title: "Lectura" },
+    //   ],
+    // },
+    component: LecturaCrearComponent,
+  },
+  {
+    path: "**",
+    redirectTo: "",
+  },
 ];
 
 @NgModule({
-  declarations: [LecturasComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [LecturasComponent, LecturaCrearComponent],
+  imports: [
+    RouterModule.forChild(routes),
+    FormsModule,
+    // ReactiveFormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+  ],
 })
-export class LecturasModule {}
+export class LecturasModule {
+  constructor() {
+
+  }
+}
