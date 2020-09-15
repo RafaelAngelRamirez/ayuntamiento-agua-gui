20a0ebf0e6f074871f6c5f7af11ce5735e4e3b04
Rafael Ramirez <rafa.yael@gmail.com>
2020-09-15 16:11:33 -0500

    Imprecion a medias para prueba en heroku

--- src/environments/config.prod.ts
+++ src/environments/config.prod.ts
@@ -1,2 +1,3 @@
-export const URL_BASE = (x: string) =>
-  `https://young-gorge-50005.herokuapp.com/${x}`;
+// export const URL_BASE = (x: string) =>
+//   `https://young-gorge-50005.herokuapp.com/${x}`;
+export const URL_BASE = (x: any) => `https://127.0.0.1:3000/${x}`;
