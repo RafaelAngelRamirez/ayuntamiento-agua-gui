400f7e4631d79ff100a420f8f819a03077335ee7
Rafael Ramirez <rafa.yael@gmail.com>
2020-10-17 11:53:28 -0500

    Servicio excel creado

--- src/app/service/excel.service.ts
+++ src/app/service/excel.service.ts
@@ -1,36 +1,36 @@
-import { Injectable } from '@angular/core'
+import { Injectable } from "@angular/core";
 
 const EXCEL_TYPE =
-  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8'
-const EXCEL_EXTENSION = '.xlsx'
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+const EXCEL_EXTENSION = ".xlsx";
 
-import * as FileSaver from 'file-saver'
-import * as XLSX from 'xlsx'
+import * as FileSaver from "file-saver";
+import * as XLSX from "xlsx";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ExcelService {
   constructor() {}
 
   public exportAsExcelFile(json: any[], excelFileName: string): void {
-    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json)
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
 
     const workbook: XLSX.WorkBook = {
       Sheets: { data: worksheet },
-      SheetNames: ['data']
-    }
+      SheetNames: ["data"],
+    };
     const excelBuffer: any = XLSX.write(workbook, {
-      bookType: 'xlsx',
-      type: 'array'
-    })
-    this.saveAsExcelFile(excelBuffer, excelFileName)
+      bookType: "xlsx",
+      type: "array",
+    });
+    this.saveAsExcelFile(excelBuffer, excelFileName);
   }
   private saveAsExcelFile(buffer: any, fileName: string): void {
-    const data: Blob = new Blob([buffer], { type: EXCEL_TYPE })
+    const data: Blob = new Blob([buffer], { type: EXCEL_TYPE });
     FileSaver.saveAs(
       data,
-      fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION
-    )
+      fileName + "_export_" + new Date().getTime() + EXCEL_EXTENSION
+    );
   }
 }
