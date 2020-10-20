3064f68ecd400274e81dc321b698e3aca19b3429
Rafael Ramirez <rafa.yael@gmail.com>
2020-10-20 17:38:33 -0500

    Algunos permisos

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,4 +1,5 @@
 import { Routes } from "@angular/router";
+import { ValidaLoginGuard } from '../guards/valida-login.guard'
 
 export const Pagesroutes: Routes = [
   { path: "", redirectTo: "/tablero", pathMatch: "full" },
@@ -6,37 +7,51 @@ export const Pagesroutes: Routes = [
     path: "tablero",
     loadChildren: () =>
       import("../dashboard/dashboard.module").then((m) => m.DashboardModule),
+      // canLoad: [ValidaLoginGuard],
+      // data: { permission: "menu:dashboard" },
   },
   {
     path: "usuario",
     loadChildren: () =>
       import("./usuario/usuario.module").then((m) => m.UsuarioModule),
+      canLoad: [ValidaLoginGuard],
+      data: { permission: "super_admin" },
   },
   {
     path: "contrato",
     loadChildren: () =>
       import("./contratos/contrato.module").then((m) => m.ContratoModule),
+      canLoad: [ValidaLoginGuard],
+      data: { permission: "login" },
   },
   {
     path: "lectura",
     loadChildren: () =>
       import("./lecturas/lecturas.module").then((m) => m.LecturasModule),
+      canLoad: [ValidaLoginGuard],
+      data: { permission: "login" },
   },
   {
     path: "parametros",
     loadChildren: () =>
       import("./parametros/parametros.module").then((m) => m.ParametrosModule),
+      canLoad: [ValidaLoginGuard],
+      data: { permission: "login" },
   },
   {
     path: "reportes",
     loadChildren: () =>
       import("./reportes/reportes.module").then((m) => m.ReportesModule),
+      canLoad: [ValidaLoginGuard],
+      data: { permission: "login" },
   },
-  {
-    path: "component",
-    loadChildren: () =>
-      import("../component/component.module").then((m) => m.ComponentsModule),
-  },
+  // {
+  //   path: "component",
+  //   loadChildren: () =>
+  //     import("../component/component.module").then((m) => m.ComponentsModule),
+  //     canLoad: [ValidaLoginGuard],
+  //     data: { permission: "login" },
+  // },
   {
     path: "**",
     redirectTo: "/app/tablero",
--- src/app/pages/usuario/usuario.module.ts
+++ src/app/pages/usuario/usuario.module.ts
@@ -6,13 +6,16 @@ import { FormsModule } from "@angular/forms";
 import { SharedModule } from "../../shared/shared.module";
 import { UsuarioCrearModificarComponent } from "./usuario-crear-modificar/usuario-crear-modificar.component";
 import { UsuarioDetalleComponent } from "./usuario-detalle/usuario-detalle.component";
+import { ValidaLoginGuard } from "../../guards/valida-login.guard";
 
 const routes: Routes = [
   {
     path: "",
+    canLoad: [ValidaLoginGuard],
     data: {
       title: "Usuario",
       urls: [{ title: "Usuario", url: "/usuario" }, { title: "Usuario" }],
+      permission: "usuario:leer:todo" ,
     },
     component: UsuarioComponent,
   },
@@ -24,6 +27,7 @@ const routes: Routes = [
         { title: "Editar usuario", url: "/editar/:id" },
         { title: "Usuario" },
       ],
+      permission: "usuario:modificar"
     },
     component: UsuarioCrearModificarComponent,
   },
@@ -32,6 +36,7 @@ const routes: Routes = [
     data: {
       title: "Editar usuario",
       urls: [{ title: "Editar usuario", url: "/crear" }, { title: "Usuario" }],
+      permission: "usuario:crear"
     },
     component: UsuarioCrearModificarComponent,
   },
