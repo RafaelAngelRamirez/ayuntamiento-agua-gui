1426ae50588afe4aeb9c9fdba93e249a897a25d9
Rafael Ramirez <rafa.yael@gmail.com>
2020-10-28 20:01:52 -0600

    Cambio de titulo

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {
     path: "",
     data: {
-      title: "Dashboard",
+      title: "Tablero",
       urls: [{ title: "Dashboard", url: "/tablero" }, { title: "Dashboard" }],
     },
     component: DashboardComponent,
