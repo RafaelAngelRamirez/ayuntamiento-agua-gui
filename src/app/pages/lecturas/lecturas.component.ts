a79303a58d3474fb77212634820431d2da2c1974
Rafael Ramirez <rafa.yael@gmail.com>
2020-11-02 08:00:57 -0600

    No se puede imprimir si uid no esta registrado a usuario

--- src/app/pages/lecturas/lecturas.component.ts
+++ src/app/pages/lecturas/lecturas.component.ts
@@ -10,6 +10,8 @@ import { FormControl } from "@angular/forms";
 import { debounceTime, distinctUntilChanged, tap } from "rxjs/operators";
 import { Router } from "@angular/router";
 import { ZebraService } from "../../services/zebra/zebra.service";
+import { UsuarioService } from "../../services/usuario.service";
+import { NotificacionesService } from "../../services/notificaciones.service";
 
 @Component({
   selector: "app-lecturas",
@@ -25,7 +27,9 @@ export class LecturasComponent implements OnInit {
   constructor(
     private zebraService: ZebraService,
     private router: Router,
-    private contratoService: ContratoService
+    private contratoService: ContratoService,
+    private usuarioService: UsuarioService,
+    private notiService: NotificacionesService
   ) {}
 
   ngOnInit(): void {
@@ -34,7 +38,18 @@ export class LecturasComponent implements OnInit {
 
     if (!this.zebraService.selected_device) {
       this.zebraService.setup();
+
+      if (!this.equipoCorrecto()) {
+        this.notiService.sweet.alerta(
+          "No estas usando la miniprinter que te fue asignada. Si es un error reportalo con el administrado. No podras tomar lecturas por el momento. "
+        );
+      }
     }
+  }
+
+  equipoCorrecto() {
+    let equipo = this.usuarioService.obtenerUsuario().dispositivo;
+    return this.zebraService.selected_device.uid === equipo;
   }
 
   registrarBuscador() {
@@ -53,6 +68,14 @@ export class LecturasComponent implements OnInit {
   }
 
   irA(contrato: Contrato) {
+    if (!this.equipoCorrecto()) {
+      this.notiService.sweet.alerta(
+        "No puedes imprimir con este equipo. Si es un error reportaselo al administrador.",
+        "Mini printer no valida"
+      );
+
+      return;
+    }
     let ruta = contrato.tomada ? "imprime" : "captura";
     this.router.navigate(["app", "lectura", ruta, contrato.Contrato]);
   }
