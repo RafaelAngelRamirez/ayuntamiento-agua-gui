51bb2dfd92d2e6e5680a6414f882b77add1de2bf
Rafael Ramirez <rafa.yael@gmail.com>
2020-12-05 11:41:17 -0600

    Fix ver primero contratos offline.

--- src/app/pages/contratos/contratos.component.ts
+++ src/app/pages/contratos/contratos.component.ts
@@ -35,7 +35,7 @@ export class ContratosComponent implements OnInit {
 
   buscador = new FormControl();
 
-  contratosOffline = false;
+  contratosOffline = true;
 
   ngOnInit(): void {
     this.registrarBuscador();
