2711663604ef0be7ed394331c217e3fe9b12aa7a
Rafael Ramirez <rafa.yael@gmail.com>
2020-12-04 21:38:38 -0600

    Calculos de tickets ordendos

---
+++ src/app/services/calculos-ticket.service.ts
@@ -0,0 +1,631 @@
+import { Injectable } from "@angular/core";
+import { Lectura, Contrato } from "./contrato.service";
+import { Lecturista, Tarifas, ParametrosSIMAPA } from '../models/usuario.model'
+
+@Injectable({
+  providedIn: "root",
+})
+export class CalculosTicketService {
+  constructor() {}
+
+  /**
+   *Obtiene los periodos (bimestrales) en base a vigencia y periodos
+   * actuales y anterioes.
+   *
+   * @param {string} vigenciaActual
+   * @param {string} periodoActual
+   * @param {number} vigenciaAnterior
+   * @param {string} periodoAnterior
+   * @returns
+   * @memberof CalculosTicketService
+   */
+  calcularPeriodo(
+    vigenciaActual: number,
+    periodoActual: number,
+    vigenciaAnterior: number,
+    periodoAnterior: number
+  ) {
+    let periodosCalculados = 0;
+
+    // No sirve una vigencia menor que 2018...
+    vigenciaAnterior =
+      vigenciaAnterior > 2018 ? vigenciaAnterior : vigenciaActual;
+    periodoAnterior = periodoAnterior > 0 ? periodoAnterior : periodoActual;
+
+    periodosCalculados =
+      // 6 periodos en bimestral
+      (vigenciaActual - vigenciaAnterior) * 6 + periodoActual - periodoAnterior;
+    return periodosCalculados < 1 ? 1 : periodosCalculados;
+  }
+
+  /**
+   *
+   * Calcula el mporte
+   * @param {Lectura} lectura
+   * @param {Contrato} contrato
+   * @param {Lecturista} lecturista
+   * @returns {number}
+   * @memberof CalculosTicketService
+   */
+  calcularImporte(
+    lectura: Lectura,
+    parametros: ParametrosSIMAPA,
+    IdTarifa: string,
+
+    vigenciaActual: number,
+    periodoActual: number,
+    vigenciaAnterior: number,
+    periodoAnterior: number
+  ): number {
+    //Obtenemos las tarifas(Parece que hay un problema con el nombre del tipo de tarifa y tiene un espacio al final. Usamos trim para evitar purrunes. )
+    let tarifas = parametros?.tarifas.filter((x) => {
+      return x.IdTarifa.trim() === IdTarifa.trim();
+    });
+
+    console.log(`tarifas`, tarifas);
+
+    //La cantidad de periodos que se estan cobrando.
+    let periodosGenerados = this.calcularPeriodo(
+      vigenciaActual,
+      periodoActual,
+      vigenciaAnterior,
+      periodoAnterior
+    );
+
+    let mesesGenerados = periodosGenerados * 2;
+
+    // El consumo que viene de la diferencia de la lectura anterior y la lectura actual.
+    let consumoActualPorMes = lectura.ConsumoMts3 / mesesGenerados;
+
+    console.log(`consumoActual`, consumoActualPorMes);
+
+    // let servicios = [];
+    // Obtenemos aguasResiduales
+    let aguasResiduales = parametros.aguasResiduales[0]
+      ? parametros.aguasResiduales[0].Porcentaje
+      : null;
+    //Obtenemos drenaje
+    let drenaje = parametros.drenaje[0]
+      ? parametros.drenaje[0].Porcentaje
+      : null;
+    // Obtenemos infrastructura
+    let infrastructura = parametros.infrastructura[0]
+      ? parametros.infrastructura[0].Porcentaje
+      : null;
+
+    console.log(`aguasResiduales`, aguasResiduales);
+    console.log(`drenaje`, drenaje);
+    console.log(`infrastructura`, infrastructura);
+
+    // //Recorremos las tarifas
+    let importeTotal = 0;
+
+    let desgloses: {
+      tarifa: Tarifas;
+      min: number;
+      max: number;
+      consumoActualMt3: number;
+      costoM3: number;
+      cuotaMinima: number;
+      importeRango: number;
+
+      metrosCalculados: number;
+    }[] = [];
+
+    for (const t of tarifas) {
+      //Si no esta igual comprobamos hasta donde haq que cobrar
+
+      let desglose = {
+        importeRango: 0,
+        metrosCalculados: 0,
+        tarifa: t,
+        min: t.ConsumoMinimo,
+        max: t.ConsmuoMaximo,
+        // El consumo actual tiene que ser divido entre la cantidad de periodos
+        // generados y el total  de periodos.
+        consumoActualMt3: consumoActualPorMes,
+        costoM3: t.CostoMt3Excedente,
+        cuotaMinima: t.CuotaMinima,
+      };
+      let subImporte = 0;
+      if (consumoActualPorMes >= t.ConsmuoMaximo) {
+        //El costo del rango
+        desglose.metrosCalculados = t.ConsmuoMaximo - t.ConsumoMinimo + 1;
+
+        subImporte =
+          desglose.metrosCalculados * t.CostoMt3Excedente + t.CuotaMinima;
+      } else {
+        if (consumoActualPorMes >= t.ConsumoMinimo) {
+          //AQUI LA HAN CAGAO con el -1
+          desglose.metrosCalculados = consumoActualPorMes - t.ConsumoMinimo + 1;
+          subImporte =
+            desglose.metrosCalculados * t.CostoMt3Excedente + t.CuotaMinima;
+        }
+      }
+      //Sumamos al importe global.
+      importeTotal += subImporte;
+
+      //Registramos el importe del rango en el desglose.
+      desglose.importeRango = subImporte;
+      desgloses.push(desglose);
+    }
+    console.log("desglose", desgloses);
+
+    // Sumamos los porcentajes de servicios
+
+    let importeAguasResiduales = aguasResiduales
+      ? importeTotal * (aguasResiduales / 100)
+      : 0;
+
+    console.log(`importeAguasResiduales`, importeAguasResiduales);
+
+    let importeInfrastructura = infrastructura
+      ? importeTotal * (infrastructura / 100)
+      : 0;
+
+    console.log(`importeInfrastructura`, importeInfrastructura);
+
+    let importeDrenaje = drenaje ? importeTotal * (drenaje / 100) : 0;
+
+    console.log(`importeDrenaje`, importeDrenaje);
+
+    let granTotal =
+      (importeTotal +
+        importeDrenaje +
+        importeInfrastructura +
+        importeAguasResiduales) *
+      mesesGenerados;
+    return granTotal;
+  }
+
+  tarfias202005:Tarifas[] = [
+    {
+      Vigencia: "2020",
+      IdTarifa: "2CH",
+      ConsumoMinimo: 0,
+      ConsmuoMaximo: 15,
+      CuotaMinima: 92.3,
+      CostoMt3Excedente: 0,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "2CH",
+      ConsumoMinimo: 16,
+      ConsmuoMaximo: 30,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 6.25,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "2CH",
+      ConsumoMinimo: 31,
+      ConsmuoMaximo: 45,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 7.67,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "2CH",
+      ConsumoMinimo: 46,
+      ConsmuoMaximo: 60,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 8.37,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "2CH",
+      ConsumoMinimo: 61,
+      ConsmuoMaximo: 75,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 9.08,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "2CH",
+      ConsumoMinimo: 76,
+      ConsmuoMaximo: 90,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 11.2,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "2CH",
+      ConsumoMinimo: 91,
+      ConsmuoMaximo: 99999999,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 11.91,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "3CH",
+      ConsumoMinimo: 0,
+      ConsmuoMaximo: 15,
+      CuotaMinima: 92.3,
+      CostoMt3Excedente: 0,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "3CH",
+      ConsumoMinimo: 16,
+      ConsmuoMaximo: 30,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 6.25,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "3CH",
+      ConsumoMinimo: 31,
+      ConsmuoMaximo: 45,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 7.67,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "3CH",
+      ConsumoMinimo: 46,
+      ConsmuoMaximo: 60,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 8.37,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "3CH",
+      ConsumoMinimo: 61,
+      ConsmuoMaximo: 75,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 9.08,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "3CH",
+      ConsumoMinimo: 76,
+      ConsmuoMaximo: 90,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 11.2,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "3CH",
+      ConsumoMinimo: 91,
+      ConsmuoMaximo: 99999999,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 11.91,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "4CH",
+      ConsumoMinimo: 0,
+      ConsmuoMaximo: 15,
+      CuotaMinima: 92.3,
+      CostoMt3Excedente: 0,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "4CH",
+      ConsumoMinimo: 16,
+      ConsmuoMaximo: 30,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 6.25,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "4CH",
+      ConsumoMinimo: 31,
+      ConsmuoMaximo: 45,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 7.67,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "4CH",
+      ConsumoMinimo: 46,
+      ConsmuoMaximo: 60,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 8.37,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "4CH",
+      ConsumoMinimo: 61,
+      ConsmuoMaximo: 75,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 9.08,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "4CH",
+      ConsumoMinimo: 76,
+      ConsmuoMaximo: 90,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 11.2,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "4CH",
+      ConsumoMinimo: 91,
+      ConsmuoMaximo: 99999999,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 11.91,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "5CH",
+      ConsumoMinimo: 0,
+      ConsmuoMaximo: 15,
+      CuotaMinima: 92.3,
+      CostoMt3Excedente: 0,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "5CH",
+      ConsumoMinimo: 16,
+      ConsmuoMaximo: 30,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 6.25,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "5CH",
+      ConsumoMinimo: 31,
+      ConsmuoMaximo: 45,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 7.67,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "5CH",
+      ConsumoMinimo: 46,
+      ConsmuoMaximo: 60,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 8.37,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "5CH",
+      ConsumoMinimo: 61,
+      ConsmuoMaximo: 75,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 9.08,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "5CH",
+      ConsumoMinimo: 76,
+      ConsmuoMaximo: 90,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 11.2,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "5CH",
+      ConsumoMinimo: 91,
+      ConsmuoMaximo: 99999999,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 11.91,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "6CH",
+      ConsumoMinimo: 0,
+      ConsmuoMaximo: 15,
+      CuotaMinima: 92.3,
+      CostoMt3Excedente: 0,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "6CH",
+      ConsumoMinimo: 16,
+      ConsmuoMaximo: 30,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 6.25,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "6CH",
+      ConsumoMinimo: 31,
+      ConsmuoMaximo: 45,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 7.67,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "6CH",
+      ConsumoMinimo: 46,
+      ConsmuoMaximo: 60,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 8.37,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "6CH",
+      ConsumoMinimo: 61,
+      ConsmuoMaximo: 75,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 9.08,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "6CH",
+      ConsumoMinimo: 76,
+      ConsmuoMaximo: 90,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 11.2,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "6CH",
+      ConsumoMinimo: 91,
+      ConsmuoMaximo: 9999999,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 11.91,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "CHU",
+      ConsumoMinimo: 0,
+      ConsmuoMaximo: 15,
+      CuotaMinima: 92.3,
+      CostoMt3Excedente: 0,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "CHU",
+      ConsumoMinimo: 16,
+      ConsmuoMaximo: 30,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 6.25,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "CHU",
+      ConsumoMinimo: 31,
+      ConsmuoMaximo: 45,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 7.67,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "CHU",
+      ConsumoMinimo: 46,
+      ConsmuoMaximo: 60,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 8.37,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "CHU",
+      ConsumoMinimo: 61,
+      ConsmuoMaximo: 75,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 9.08,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "CHU",
+      ConsumoMinimo: 76,
+      ConsmuoMaximo: 90,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 11.2,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "CHU",
+      ConsumoMinimo: 91,
+      ConsmuoMaximo: 99999999,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 11.91,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "COM",
+      ConsumoMinimo: 0,
+      ConsmuoMaximo: 25,
+      CuotaMinima: 162.96,
+      CostoMt3Excedente: 0,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "COM",
+      ConsumoMinimo: 26,
+      ConsmuoMaximo: 40,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 9.79,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "COM",
+      ConsumoMinimo: 41,
+      ConsmuoMaximo: 55,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 11.2,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "COM",
+      ConsumoMinimo: 56,
+      ConsmuoMaximo: 70,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 11.91,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "COM",
+      ConsumoMinimo: 71,
+      ConsmuoMaximo: 85,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 12.62,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "COM",
+      ConsumoMinimo: 86,
+      ConsmuoMaximo: 100,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 14.04,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "COM",
+      ConsumoMinimo: 101,
+      ConsmuoMaximo: 99999999,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 14.75,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "ESC",
+      ConsumoMinimo: 0,
+      ConsmuoMaximo: 15,
+      CuotaMinima: 92.3,
+      CostoMt3Excedente: 0,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "ESC",
+      ConsumoMinimo: 16,
+      ConsmuoMaximo: 30,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 6.25,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "ESC",
+      ConsumoMinimo: 31,
+      ConsmuoMaximo: 45,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 7.67,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "ESC",
+      ConsumoMinimo: 46,
+      ConsumoMaximo: 60,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 8.37,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "ESC",
+      ConsumoMinimo: 61,
+      ConsumoMaximo: 75,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 9.08,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "ESC",
+      ConsumoMinimo: 76,
+      ConsumoMaximo: 90,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 11.2,
+    },
+    {
+      Vigencia: "2020",
+      IdTarifa: "ESC",
+      ConsumoMinimo: 91,
+      ConsumoMaximo: 9999999,
+      CuotaMinima: 0,
+      CostoMt3Excedente: 11.91,
+    },
+  ];
+}
