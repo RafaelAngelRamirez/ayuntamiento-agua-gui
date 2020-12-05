43fd9ecd2c7b0d3bd185317e4b3d644075e2a972
Rafael Ramirez <rafa.yael@gmail.com>
2020-12-04 21:07:39 -0600

    Debe incluir por lo menos uno de los permisos

--- src/app/pipes/tiene-permiso.pipe.ts
+++ src/app/pipes/tiene-permiso.pipe.ts
@@ -7,9 +7,25 @@ import { UsuarioService } from "../services/usuario.service";
 export class TienePermisoPipe implements PipeTransform {
   constructor(private usuarioService: UsuarioService) {}
 
-  transform(value: string): boolean {
+  /**
+   *Debe tener por lo menos uno de los permisos solicitados
+   *
+   * @param {string[]} args
+   * @returns {boolean}
+   * @memberof TienePermisoPipe
+   */
+  transform(permiso:string, ...args: string[]): boolean {
     let usuario = this.usuarioService.obtenerUsuario();
 
-    return usuario.permissions.includes(value);
+    
+    if (usuario.permissions.includes(permiso)) return true;
+
+    for (let i = 0; i < args.length; i++) {
+      const p = args[i];
+
+      if (usuario.permissions.includes(p)) return true;
+    }
+
+    return false;
   }
 }
