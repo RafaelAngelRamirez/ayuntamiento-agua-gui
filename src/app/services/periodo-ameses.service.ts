906359540483a259ab1288db952607279cbb3814
Rafael Ramirez <rafa.yael@gmail.com>
2020-12-12 08:44:30 -0600

    Equivalencia de periodos en nombre a publico y readonly

--- src/app/services/periodo-ameses.service.ts
+++ src/app/services/periodo-ameses.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from "@angular/core";
   providedIn: "root",
 })
 export class PeriodoAMesesService {
-  private equivalencia = [
+  public readonly equivalencia = [
     "Enero-Febrero",
     "Marzo-Abril",
     "Mayo-Junio",
