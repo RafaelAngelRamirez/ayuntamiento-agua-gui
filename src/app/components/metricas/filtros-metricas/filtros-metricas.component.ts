efb07a114a55c229f39388a495c4464baafbf0ce
Rafael Ramirez <rafa.yael@gmail.com>
2020-12-14 08:31:16 -0600

    Mejora de filtros

--- src/app/components/metricas/filtros-metricas/filtros-metricas.component.ts
+++ src/app/components/metricas/filtros-metricas/filtros-metricas.component.ts
@@ -23,13 +23,14 @@ export class FiltrosMetricasComponent implements OnInit {
     private periodoAMesesService: PeriodoAMesesService
   ) {}
   form = new FormGroup({
-    fechaLimiteInferior: new FormControl([""]),
-    fechaLimiteSuperior: new FormControl([""]),
-    vigencia: new FormControl([new Date().getFullYear()]),
-    periodo: new FormControl([""]),
-    lecturista: new FormControl([""]),
-    calle: new FormControl([""]),
-    colonia: new FormControl([""]),
+    fechaLimiteInferior: new FormControl(""),
+    fechaLimiteSuperior: new FormControl(""),
+    vigencia: new FormControl(new Date().getFullYear()),
+    periodo: new FormControl(""),
+    lecturista: new FormControl(""),
+    calle: new FormControl(""),
+    colonia: new FormControl(""),
+    poblacion: new FormControl(""),
   });
 
   ngOnInit(): void {
@@ -39,18 +40,26 @@ export class FiltrosMetricasComponent implements OnInit {
 
     let contador = 0;
     this.periodos = this.periodoAMesesService.equivalencia.map((x) => {
+      contador++
       return {
         periodo: x,
         // Los periodos empiezan desde 1
-        valor: contador++,
+        valor: contador,
       };
     });
 
     // Emitimos los eventos cuando haya un cambio en el formulario.
     this.form.valueChanges.subscribe((values) => {
-      console.log(values);
       this.submit(values);
     });
+
+    this.parametrosService.cargarPeriodoVigencia().subscribe((datos)=>{
+      
+      this.form.get("vigencia")?.setValue(datos.vigencia)
+      this.form.get("periodo")?.setValue(datos.periodo)
+
+    } )
+
   }
 
   submit(modelo: any) {
@@ -62,11 +71,16 @@ export class FiltrosMetricasComponent implements OnInit {
         let esFecha = false;
         if (x === "fechaLimiteInferior" || x === "fechaLimiteSuperior")
           esFecha = true;
+        //Parametros en nulo no pasan
+        if (!modelo[x]) return null;
         return `${x}=${esFecha ? this.convertirFecha(modelo[x]) : modelo[x]}`;
       })
+      .filter((x) => x)
       .join("&");
 
-    this.cadena.emit(encodeURI("?" + cadena));
+    cadena = encodeURI("?" + cadena);
+    console.log(cadena);
+    this.cadena.emit(cadena);
   }
 
   convertirFecha(f: string): string {
